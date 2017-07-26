e4400314f132df87d181af329a4001bd32d25c0d
akahige <akahigeg@gmail.com>
2017-07-26 19:59:12 +0900

    add taxonomy support.

--- src/index.php
+++ src/index.php
@@ -16,7 +16,26 @@ if (!array_key_exists('wp-cpt-json', $GLOBALS)) {
       $post_types = yaml_parse_file(plugin_dir_path(__FILE__) . '/post_types.yml');
 
       foreach($post_types as $post_type_name => $options) {
-    	  register_post_type($post_type_name, $options);
+      	# taxonomies
+      	$taxonomies = array();
+      	if (array_key_exists('taxonomies', $options)) {
+          $taxonomies = $options["taxonomies"];
+          $taxonomy_names = array();
+          foreach ($taxonomies as $i => $taxonomy_name_and_args) {
+          	foreach ($taxonomy_name_and_args as $name => $args) {
+              $taxonomy_names[] = $name;
+          	}
+          }
+          $options["taxonomies"] = $taxonomy_names;
+      	}
+
+    	register_post_type($post_type_name, $options);
+
+    	foreach ($taxonomies as $i => $taxonomy_name_and_args) {
+          foreach ($taxonomy_name_and_args as $name => $args) {
+            register_taxonomy($name, $post_type_name, $args);
+          }
+    	}
       }
 
   	}

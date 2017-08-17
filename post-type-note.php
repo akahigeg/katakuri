842c6ae2d25b588ae544b3421b6526698a74712e
akahige <akahigeg@gmail.com>
2017-08-17 19:30:06 +0900

    create Util.php.

--- post-type-note.php
+++ post-type-note.php
@@ -164,26 +164,9 @@ if (!array_key_exists('post-type-note', $GLOBALS)) {
     }
   }
 
-  class PostTypeNoteUtil {
-    public static function underscore($str) {
-      return ltrim(strtolower(preg_replace('/[A-Z]/', '_\0', $str)), '_');
-    }
-
-    public static function camelize($str) {
-      return lcfirst(strtr(ucwords(strtr($str, array('_' => ' '))), array(' ' => '')));
-    }
-
-    public static function pascalize($str) {
-      return ucfirst(strtr(ucwords(strtr($str, array('_' => ' '))), array(' ' => '')));
-    }
-
-    public function __call($name, $args) {
-      echo $name;
-    }
-  }
-
   $include_path = plugin_dir_path(__FILE__) . 'includes';
   require_once($include_path . '/PostTypeNoteFormRenderer.php');
+  require_once($include_path . '/PostTypeNoteUtil.php');
 
   $GLOBALS['post-type-note'] = new PostTypeNote();
   add_action('init', 'PostTypeNote::init');

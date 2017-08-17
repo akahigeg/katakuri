4a85e19f538d6f82ef3572045e8e662fb7f7c266
akahige <akahigeg@gmail.com>
2017-08-17 19:27:04 +0900

    create PostTypeNoteRenderer.php and move that class to that file.

--- post-type-note.php
+++ post-type-note.php
@@ -164,105 +164,6 @@ if (!array_key_exists('post-type-note', $GLOBALS)) {
     }
   }
 
-  class PostTypeNoteFormRenderer {
-    public static function renderText($field_name, $saved_value, $options) {
-      self::renderLabel($field_name, $options);
-
-      $size = isset($options['size']) ? $options['size'] : '40';
-      echo '<input name="' . $field_name . '" type="text" value="' . $saved_value . '" size="' . $size . '">';
-    }
-
-    public static function renderCheckbox($field_name, $saved_value, $options) {
-      self::renderLabel($field_name, $options);
-
-      $saved_values = maybe_unserialize($saved_value);
-      foreach ($options['values'] as $value) {
-        if (is_array($value)) {
-          $option_value = array_keys($value)[0];
-          $option_label = array_values($value)[0];
-        } else {
-          $option_value = $value;
-          $option_label = $value;
-        }
-        if (is_array($saved_values) && in_array($option_value, $saved_values)) {
-          $checked = 'checked';
-        } else {
-          $checked = '';
-        }
-        echo '<label>';
-        echo '<input type="checkbox" name="' . $field_name . '[]" value="' . $option_value . '" ' . $checked . '>';
-        echo $option_label . '</label> ';
-      }
-    }
-
-    public static function renderRadio($field_name, $saved_value, $options) {
-      self::renderLabel($field_name, $options);
-
-      $checks = array();
-      foreach ($options['values'] as $value) {
-        if ($saved_value == $value) {
-          $checks[] = $value;
-        }
-      }
-      foreach ($options['values'] as $value) {
-        if ($saved_value == $value || (count($checks) == 0 && $value == $options['default'])) {
-          $checked = 'checked';
-        } else {
-          $checked = '';
-        }
-        echo '<label>';
-        echo '<input type="radio" name="' . $field_name . '" value="' . $value . '" ' . $checked . '>';
-        echo $value . '</label> ';
-      }
-    }
-
-    public static function renderTextarea($field_name, $saved_value, $options) {
-      self::renderLabel($field_name, $options);
-
-      $rows = isset($options['rows']) ? $options['rows'] : '5';
-      $cols = isset($options['cols']) ? $options['cols'] : '40';
-      echo '<textarea name="' . $field_name . '" rows="' . $rows . '" cols="' . $cols . '">' . $saved_value . '</textarea>';
-    }
-
-    public static function renderSelect($field_name, $saved_value, $options) {
-      self::renderLabel($field_name, $options);
-
-      $saved_values = maybe_unserialize($saved_value);
-
-      $size = isset($options['size']) ? 'size="' . $options['size'] . '"' : '';
-      $width_style = isset($options['width']) ? 'style="width:' . $options['width'] . 'px;"' : '';
-      $multiple = isset($options['multiple']) && $options['multiple'] == true ? 'multiple' : '';
-
-      echo '<select name="' . $field_name . '[]" ' . $size . ' ' . $width_style . ' ' . $multiple . '>';
-      self::renderOptions($saved_values, $options);
-      echo '</select>';
-    }
-
-    public static function renderOptions($saved_values, $options) {
-      foreach ($options['values'] as $value) {
-        if (is_array($value)) {
-          $option_value = array_keys($value)[0];
-          $option_label = array_values($value)[0];
-        } else {
-          $option_value = $value;
-          $option_label = $value;
-        }
-        if (in_array($option_value, $saved_values)) {
-          $selected = 'selected';
-        } else {
-          $selected = '';
-        }
-        echo '<option value="' . $option_value . '" ' . $selected . '>' . $option_label . '</option>';
-      }
-    }
-
-    public static function renderLabel($field_name, $options) {
-      if (isset($options['label'])) {
-        echo '<label for="' . $field_name . '">' . $options['label'] . '</label>';
-      }
-    }
-  }
-  
   class PostTypeNoteUtil {
     public static function underscore($str) {
       return ltrim(strtolower(preg_replace('/[A-Z]/', '_\0', $str)), '_');
@@ -281,6 +182,9 @@ if (!array_key_exists('post-type-note', $GLOBALS)) {
     }
   }
 
+  $include_path = plugin_dir_path(__FILE__) . 'includes';
+  require_once($include_path . '/PostTypeNoteFormRenderer.php');
+
   $GLOBALS['post-type-note'] = new PostTypeNote();
   add_action('init', 'PostTypeNote::init');
   add_action('add_meta_boxes', 'PostTypeNote::addMetaBoxes');
@@ -288,6 +192,8 @@ if (!array_key_exists('post-type-note', $GLOBALS)) {
 
   add_action('manage_posts_columns', 'PostTypeNote::manageColumns');
   add_action('manage_posts_custom_column', 'PostTypeNote::manageCustomColumns', 10, 2);
+  // add_filter('manage_edit-hoge_sortable_columns', 'PostTypeNote::hoge');
+
 }
 
 /*

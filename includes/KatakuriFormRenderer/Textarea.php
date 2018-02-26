482daf06b0b1336807070001197e1e79712f7784
akahigeg <akahigeg@gmail.com>
2018-02-26 21:57:36 +0900

    enable to add styles and classes to textarea.

--- includes/KatakuriFormRenderer/Textarea.php
+++ includes/KatakuriFormRenderer/Textarea.php
@@ -7,9 +7,11 @@ class KatakuriFormRendererTextarea {
   public static function build($field_name, $saved_value, $options) {
     $html = KatakuriFormRendererLabel::build($field_name, $options);
 
+    $style_and_class = KatakuriFormRendererHelperStyleAndClass::forInput($options);
+
     $rows = isset($options['rows']) ? $options['rows'] : '5';
     $cols = isset($options['cols']) ? $options['cols'] : '40';
-    $html .= '<textarea name="' . $field_name . '" rows="' . $rows . '" cols="' . $cols . '">' . $saved_value . '</textarea>';
+    $html .= '<textarea name="' . $field_name . '" rows="' . $rows . '" cols="' . $cols . '" ' . $style_and_class . '>' . $saved_value . '</textarea>';
 
     return $html;
   }

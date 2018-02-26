6cfdb51680f93e0a94cd596c91daf2e481623a49
akahigeg <akahigeg@gmail.com>
2018-02-26 21:39:03 +0900

    add label options to specify class and style.

--- includes/KatakuriFormRenderer/Label.php
+++ includes/KatakuriFormRenderer/Label.php
@@ -5,8 +5,18 @@ class KatakuriFormRendererLabel {
   }
 
   public static function build($field_name, $options) {
-    if (isset($options['label'])) {
-      return '<label for="' . $field_name . '" style="padding-right: 8px; vertical-align: middle;">' . $options['label'] . '</label>';
+    if (isset($options['label']) == false) {
+      return;
     }
+
+    $attrs = array();
+    if (isset($options['label_class'])) {
+      $attrs[] = 'class="' . $options['label_class'] . '"';
+    }
+    if (isset($options['label_style'])) {
+      $attrs[] = 'style="' . $options['label_style'] . '"';
+    }
+
+    return '<label for="' . $field_name . '" ' . implode(' ', $attrs) . '>' . $options['label'] . '</label>';
   }
 }
